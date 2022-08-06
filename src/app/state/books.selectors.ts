cb5fffdd10b9e556da3f91a9187ee12be83da954
Sunny S <sunny.sharma@gorillagroup.com>
2022-08-05 20:06:15 -0700

    Added ngx store to manage the state for fetching the data and manuplating

---
+++ src/app/state/books.selectors.ts
@@ -0,0 +1,13 @@
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { Book } from '../book-list/books.model';
+
+export const selectBooks = createFeatureSelector<ReadonlyArray<Book>>('books');
+export const selectCollectionState = createFeatureSelector<ReadonlyArray<string>>('collection');
+export const selectBookCollection = createSelector(
+    selectBooks,
+    selectCollectionState,
+    (books, collection) => {
+        console.log('Hi')
+        return collection.map((id) => books.find((book) => book.id == id))
+    }
+)
